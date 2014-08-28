4158e75640fef527c52eeea61aaeef03a0a42c5f
Mark Snijder <snijderd@gmail.com>
2014-08-28 10:20:23 +0200

    because

--- elysian/com/github/lyokofirelyte/Elysian/Commands/ElyStaff.java
+++ elysian/com/github/lyokofirelyte/Elysian/Commands/ElyStaff.java
@@ -52,6 +52,56 @@ public class ElyStaff implements Listener {
 			 main.s(p, "playerNotFound");
 		 }
 	 }
+	 	 
+	 @DivCommand(perm = "wa.staff.admin", aliases = {"rocketban"}, desc = "Rocketban someone!", help = "/rocketban <player> <reason>", player = true, min = 2)
+	 public void onRocketBan(Player p, String[] args){
+		 
+
+		 
+		 if(main.isOnline(args[0])){
+			 main.api.schedule(this, "beginmessage", 10L, "beginmessage");
+			 main.api.schedule(this, "banmessage", 20L, "banmessage", 0);
+			 main.api.schedule(this, "banmessage", 30L, "banmessage", 20);
+			 main.api.schedule(this, "banmessage", 40L, "banmessage", 40);
+			 main.api.schedule(this, "banmessage", 50L, "banmessage", 60);
+			 main.api.schedule(this, "banmessage", 60L, "banmessage", 80);
+			 main.api.schedule(this, "banmessage", 70L, "banmessage", 100);
+			 main.api.schedule(this, "foundplayer", 80L, "foundplayer", args[0]);
+
+			 main.api.schedule(this, "rocketban", 100L, "rocketban", p, args[0], args);
+
+		 }
+
+	 }
+	 
+	 public void banmessage(int percentage){
+		 for(Player p : Bukkit.getOnlinePlayers()){
+			 main.s(p, "[Scanning for X-rayers " + percentage + "% ]");
+		 }
+	 }
+	 
+	 public void beginmessage(){
+		 for(Player p : Bukkit.getOnlinePlayers()){
+			 main.s(p, "[Scanning for X-rayers]");
+		 }
+	 }
+	 
+	 public void foundplayer(String name){
+		 for(Player p : Bukkit.getOnlinePlayers()){
+			 main.s(p, "[1 result found! " + name + " ]");
+		 } 
+	 }
+	 
+	 public void rocketban(Player p, String name, String[] params){
+		 StringBuilder reason = new StringBuilder();
+		 for(int i = 0; i < params.length; i++){
+			 try{
+				 reason.append(params[i + 1] + " ");
+			 }catch(Exception e){}
+		 }
+		 
+		p.performCommand("ban " + name + " " + reason.toString());
+	 }
 	 
 	 @DivCommand(perm = "wa.staff.mod2", aliases = {"clear"}, desc = "Clear items on floor (and monsters)", help = "/clear <radius>", player = true, min = 1)
 	 public void onClear(Player p, String[] args){
