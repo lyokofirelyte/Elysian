2363e8aeed34a55ea28554e12ccb766abe6f8f31
unknown <kyle.jankowski@hotmail.com>
2014-10-02 02:25:13 -0500

    Recoded Friendly reminder class

--- elysian/com/github/lyokofirelyte/Elysian/Gui/GuiFriendlyReminder.java
+++ elysian/com/github/lyokofirelyte/Elysian/Gui/GuiFriendlyReminder.java
@@ -7,6 +7,7 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 
 import com.github.lyokofirelyte.Divinity.DivGui;
+import com.github.lyokofirelyte.Divinity.Storage.DPI;
 import com.github.lyokofirelyte.Divinity.Storage.DivinityPlayer;
 import com.github.lyokofirelyte.Elysian.Elysian;
 
@@ -27,26 +28,26 @@ public class GuiFriendlyReminder extends DivGui{
 	@Override
 	public void create() {
 		
-		addButton(0, createItem("&aAnimal Killing", new String[] { "&3Toggle animal", "&3killing msgs" , main.friendlyReminder.getStatus(player, "FK")}, Material.LEATHER, 1));
-		addButton(1, createItem("&aCreeper Holes", new String[] { "&3Toggle creeper", "&3hole msgs" , main.friendlyReminder.getStatus(player, "CH")}, Material.SKULL_ITEM, 1, 4));
-		addButton(2, createItem("&aCrop Replanting", new String[] { "&3Toggle crop re-", "&3planting msgs" , main.friendlyReminder.getStatus(player, "CR")}, Material.DIAMOND_HOE, 1));
-		addButton(3, createItem("&aTree Replanting", new String[] { "&3Toggle tree re-", "&3planting msgs" , main.friendlyReminder.getStatus(player, "TR")}, Material.SAPLING, 0));
+		addButton(0, createItem("&aAnimal Killing", new String[] { "&3Toggle animal", "&3killing msgs" , main.friendlyReminder.getStatus(player, DPI.FR_FK_TOGGLE)}, Material.LEATHER, 1));
+		addButton(1, createItem("&aCreeper Holes", new String[] { "&3Toggle creeper", "&3hole msgs" , main.friendlyReminder.getStatus(player, DPI.FR_CH_TOGGLE)}, Material.SKULL_ITEM, 1, 4));
+		addButton(2, createItem("&aCrop Replanting", new String[] { "&3Toggle crop re-", "&3planting msgs" , main.friendlyReminder.getStatus(player, DPI.FR_CR_TOGGLE)}, Material.DIAMOND_HOE, 1));
+		addButton(3, createItem("&aTree Replanting", new String[] { "&3Toggle tree re-", "&3planting msgs" , main.friendlyReminder.getStatus(player, DPI.FR_TR_TOGGLE)}, Material.SAPLING, 0));
 		addButton(8, createItem("&bRETURN", new String[] { "&b< < <" }, Enchantment.DURABILITY, 10, Material.FLINT));
 	}
 
 	@Override
 	public void actionPerformed(Player p) {
 		switch(slot) {
-		case 0: main.friendlyReminder.toggleFK(main.getDivPlayer(p));
+		case 0: main.friendlyReminder.Toggle(main.getDivPlayer(p), DPI.FR_FK_TOGGLE);
 		create();
 		break;
-		case 1: main.friendlyReminder.toggleCH(main.getDivPlayer(p));
+		case 1: main.friendlyReminder.Toggle(main.getDivPlayer(p), DPI.FR_CH_TOGGLE);
 		create();
 		break;
-		case 2: main.friendlyReminder.toggleCR(main.getDivPlayer(p));
+		case 2: main.friendlyReminder.Toggle(main.getDivPlayer(p), DPI.FR_CR_TOGGLE);
 		create();
 		break;
-		case 3: main.friendlyReminder.toggleTR(main.getDivPlayer(p));
+		case 3: main.friendlyReminder.Toggle(main.getDivPlayer(p), DPI.FR_TR_TOGGLE);
 		create();
 		break;
 		case 8: main.invManager.displayGui(p, this.parent);
